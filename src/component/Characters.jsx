2cbc6f9a5d092f8df95d3a3824bbcc0c2eaf4b43
sumancoding <progress.suman@gmail.com>
2022-05-06 01:46:01 +0300

    display fecthed data with mui

--- src/component/Characters.jsx
+++ src/component/Characters.jsx
@@ -1,8 +1,17 @@
-import React, { useEffect } from "react";
-import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Card, styled, Grid } from "@mui/material";
 import { fetchData } from "./service/api";
 
+//when using html inside styledComponent we use '' and ({}). The peroperties are written in camelCase not lower block
+const Image = styled("img")({
+  height: 300,
+  width: "100%",
+  objectFit: "cover",
+});
+
 const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+
   useEffect(() => {
     getData();
   }, []);
@@ -10,10 +19,20 @@ const Characters = () => {
   const getData = async () => {
     let response = await fetchData();
     console.log(response);
+    setCharacters(response.data);
   };
   return (
-    <Box>
-      <h1>Characters</h1>
+    <Box sx={{ m: "5px" }}>
+      <Grid container spacing={4}>
+        {" "}
+        {characters.map((character) => (
+          <Grid item xs={12} md={4} lg={3}>
+            <Card>
+              <Image src={character.img} alt="img" />
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
